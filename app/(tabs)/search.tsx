8877779e62ede11001cfd3d4eaf682e7577659cb
Mohamedhadhoud1 <muhammed195837@feng.bu.edu.eg>
2025-11-19 03:45:39 +0200

    testing

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -41,6 +41,7 @@ const Search = () => {
     return () => clearTimeout(timeoutId);
   }, [searchQuery]);
 
+  
   useEffect(() => {
            // Call updateSearchCount only if there are results
         if (movies?.length! > 0 && movies?.[0]) {
